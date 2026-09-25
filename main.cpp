c2537acce6b41209d5ea155cf815f38c821bd1b8
Samridhi Bansal <bansalsamridhi10@gmail.com>
2026-09-25 21:43:18 +0530

    feat: add percentage and grade calculation

--- main.cpp
+++ main.cpp
@@ -11,5 +11,22 @@ int main(){
         total+=marks[i];
     }
     cout << "Total Marks: " << total << " / " << n * 100 << endl;
+    float percentage = total / (n * 100) * 100;
+
+char grade;
+
+if (percentage >= 90)
+    grade = 'A';
+else if (percentage >= 80)
+    grade = 'B';
+else if (percentage >= 70)
+    grade = 'C';
+else if (percentage >= 60)
+    grade = 'D';
+else
+    grade = 'F';
+
+cout << "Percentage: " << percentage << "%" << endl;
+cout << "Grade: " << grade << endl;
     return 0;
 }
